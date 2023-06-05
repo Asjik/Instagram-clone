f19b6a0cc6466ce860a195356abfbbcd5e16191b
Asjik <forsaken65-1993@o2.pl>
2023-06-05 21:22:55 +0200

    Updated SugnUp view with RHF

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -7,6 +7,7 @@ import {
 } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function SignUp() {
   const [email, setEmail] = useState("");
@@ -16,8 +17,14 @@ function SignUp() {
   const navigateSignup = useNavigate();
   const handleClickSignup = () => navigateSignup("/");
 
-  const handleSignUp = (event) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSignUp = () => {
+    //event.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
       .then((authUser) => {
         signInWithEmailAndPassword(auth, email, password).then(
@@ -44,25 +51,45 @@ function SignUp() {
         src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
         alt="Instagram logo"
       ></img>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        type="email"
-        placeholder="Email"
-        value={email}
-      ></input>
-      <input
-        onChange={(e) => setUserName(e.target.value)}
-        type="text"
-        placeholder="User name"
-        value={userName}
-      ></input>
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        placeholder="Password"
-        value={password}
-      ></input>
-      <button onClick={handleSignUp}>Sign Up</button>
+      <form onSubmit={handleSubmit(handleSignUp)}>
+        <input
+          placeholder="Email"
+          type="email"
+          {...register("email", {
+            required: true,
+            onChange: (e) => {
+              setEmail(e.target.value);
+            },
+            // pattern:
+            //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+        ></input>
+        {errors.email && <p>Please check the Email</p>}
+        <input
+          placeholder="User name"
+          type="text"
+          {...register("name", {
+            required: true,
+            maxLength: 10,
+            onChange: (e) => {
+              setUserName(e.target.value);
+            },
+          })}
+        ></input>
+        <input
+          placeholder="Password"
+          type="password"
+          {...register("password", {
+            required: true,
+            maxLength: 10,
+            onChange: (e) => {
+              setPassword(e.target.value);
+            },
+            // pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/,
+          })}
+        ></input>
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 }
